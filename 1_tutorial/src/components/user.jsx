cfb577535e1ddca579e78cf3cc797e0a29c8acfb
mert_gvcn <mertguvercin2942@gmail.com>
2023-06-25 02:58:54 +0300

    props

--- 1_tutorial/src/components/user.jsx
+++ 1_tutorial/src/components/user.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 
+//? class component -> state ve prop alabilen dinamik componentlardır 
 //? rcc + tab yapınca bu temel yapı otomatik oluşuyor
 
 class User extends Component {
   render() {
+
+    //Destructing ile bu class'a ait propsları çektik. 
+    const {name,department,salary} = this.props; 
+
     return (
       <div>
-        <form>
-            <input type="text"/>
-            <button>Gönder</button>
-        </form>
+        <ul>
+            <li>Name       : {name}</li>
+            <li>Department : {department}</li>
+            <li>Salary     : {salary}</li>
+        </ul>
       </div>
     )
   }
