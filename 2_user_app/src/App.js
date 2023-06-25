4a9ed2622f35bab2853ccce42e5f4e941d099a5a
mert_gvcn <mertguvercin2942@gmail.com>
2023-06-25 16:04:20 +0300

    user app (prop drilling method)

--- 2_user_app/src/App.js
+++ 2_user_app/src/App.js
@@ -1,23 +1,39 @@
-import './App.css';
-import Navbar from './components/navbar.jsx';
-import User from './components/user.jsx';
+import React, { Component } from 'react'
+import Navbar from './components/navbar'
+import Users from './components/users'
 
-function App() {
-  return (
-    <div>
-      <Navbar title="USER APP" />
-      <User
-        name="Mert Güvercin"
-        department="Yazılım Mühendisliği"
-        salary="15000 TL"
-      />
-      <User
-        name="Ali Güvercin"
-        department="İnsan Kaynakları"
-        salary="12000 TL"
-      />
-    </div>
-  )
+class App extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        name: "Mert Güvercin",
+        department: "Yazılım Mühendisliği",
+        salary: "15000 TL"
+      },
+      {
+        id: 2,
+        name: "Ali Güvercin",
+        department: "İnsan Kaynakları",
+        salary: "1200 TL"
+      }
+
+    ]
+  }
+
+  render() {
+    const users = this.state.users //stateteki usersları aldık
+
+    return (
+      <div>
+        <div>
+          <Navbar title="USER APP" />
+          <Users users= {users}/> {/*userları users componentine yazdırıcaz*/}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
